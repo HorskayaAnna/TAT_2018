a4ce6620ab9fff69fd4c727e32aaa70de0333525
HorskayaAnna <Horskaya.Anna@gmail.com>
2018-03-02 18:08:21 +0300

    Task DEV-1

---
+++ DEV-1/DEV-1/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DevTask1
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Write("Wrong number of arguments.");
+            }
+            else
+            {
+                MaxNumberOfTheSameConsecutiveSymbolsFinder finder = new MaxNumberOfTheSameConsecutiveSymbolsFinder(args[0]);
+                Console.WriteLine($"Max value of sequent symbols is: {finder.GetNumberOfTheSameConsecutiveSymbols()}");
+            }
+        }
+    }
+}
---
+++ DEV-1/DEV-1/MaxLengthSameStringFinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DevTask1
+{
+    /// <summary>
+    /// This class contains method, which calculates
+    /// max number of the same consecutive symbols  
+    /// </summary>
+    class MaxNumberOfTheSameConsecutiveSymbolsFinder
+    {
+        string processedLine;
+        /// <summary>
+        /// Initializer of MaxNumberOfTheSameConsecutiveSymbolsFinder's instance
+        /// </summary>
+        /// <param name="arg"></param>
+        public MaxNumberOfTheSameConsecutiveSymbolsFinder(string arg)
+        {
+            processedLine = arg;
+        }
+        /// <summary>
+        /// This metod calculates 
+        /// max number of the same consecutive symbols
+        /// </summary>
+        /// <returns>
+        /// Returns max numbers of the same consecutive symbols
+        /// </returns>
+        public int GetNumberOfTheSameConsecutiveSymbols()
+        {
+            int maxQuantityOfIdenticalSymbols = 0;
+            int counterOfIdenticalSymbols = 1;
+            for (int i = 0; i < processedLine.Length - 1; i++)
+            {
+                if (processedLine[i].Equals(processedLine[i + 1]))
+                {
+                    counterOfIdenticalSymbols++;
+                    if (counterOfIdenticalSymbols > maxQuantityOfIdenticalSymbols)
+                    {
+                        maxQuantityOfIdenticalSymbols = counterOfIdenticalSymbols;
+                    }
+                }
+                else
+                {
+                    counterOfIdenticalSymbols = 1;
+                }
+            }
+            return maxQuantityOfIdenticalSymbols;
+        }
+    }
+}
