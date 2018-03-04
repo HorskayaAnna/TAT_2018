874615d0711710279e8431a00fa196202a5552a1
HorskayaAnna <Horskaya.Anna@gmail.com>
2018-03-04 13:59:30 +0300

    Task 2

---
+++ DEV-2/DEV-2/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace DevTask2
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input string, please");
+            StringBuilder inputString = new StringBuilder(Console.ReadLine());
+            if (inputString.Length > 0)
+            {
+                GetStringWithEvenSymbols delete = new GetStringWithEvenSymbols(inputString);
+                delete.DeleteOddSymbols();
+            }
+            else Console.WriteLine("Error:Empty string");
+        }
+    }
+}
---
+++ DEV-2/DEV-2/GetStringWithEvenSymbols.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace DevTask2
+{
+    /// <summary>
+    /// This class contains method, which delete odd symbols 
+    /// in string and output this string
+    /// </summary>
+    class GetStringWithEvenSymbols
+    {
+        StringBuilder processedString;
+
+        /// <summary>
+        /// Initializer of GetStringWithEvenSymbols's instance
+        /// </summary>
+        /// <param name="arg">
+        /// string taken from the console
+        /// </param>
+        public GetStringWithEvenSymbols(StringBuilder arg)
+        {
+            processedString = arg;
+        }
+
+        /// <summary>
+        /// This metod Delete Odd Symbols in string
+        /// </summary>
+        public void DeleteOddSymbols()
+        {
+            processedString = processedString.Replace(" ", "");
+            StringBuilder bufferString = new StringBuilder(10);
+            int position=0;
+            for (int i = 1; i < processedString.Length;i+=2)
+            {
+                bufferString.Insert(position, processedString[i]);
+                position++;
+            }
+            Console.WriteLine($"String with even symbols : {bufferString}");
+        }
+    }
+}
