6fa89e44f34c5fe1dc636b31eca0b7710e9fd58d
HorskayaAnna <Horskaya.Anna@gmail.com>
2018-03-16 12:17:38 +0300

    TaskDEV2 unit tests xunit

---
+++ DEV-2/DEV2Tests/GetterStringWithEvenSymbols.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using Xunit;
+using DevTask2;
+
+namespace DEV2Tests
+{
+
+    public class GetterStringWithEvenSymbolsTests
+    {
+        [Fact]
+        public void GetEvenPositionsStringBuilder_IsCorrectWorkOfMethod()
+        {
+            StringBuilder inputString = new StringBuilder("qwqwqw");
+            string expected = "qqq";
+
+            GetterStringWithEvenSymbols evener = new GetterStringWithEvenSymbols(inputString);
+            string actual = evener.GetEvenPositionsStringBuilder().ToString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetEvenPositionsStringBuilder_IsEmptyString()
+        {
+            StringBuilder inputString = new StringBuilder(string.Empty);
+            string expected = string.Empty;
+
+            GetterStringWithEvenSymbols evener = new GetterStringWithEvenSymbols(inputString);
+            string actual = evener.GetEvenPositionsStringBuilder().ToString();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
