7b308c7232539b4ffabc2fed718d6a7683c471fb
HorskayaAnna <Horskaya.Anna@gmail.com>
2018-03-16 17:09:03 +0300

    add xUnit test DevTask2

--- DEV-2/DEV2Tests/GetterStringWithEvenSymbols.cs
+++ DEV-2/DEV2Tests/GetterStringWithEvenSymbols.cs
@@ -4,13 +4,24 @@ using DevTask2;
 
 namespace DEV2Tests
 {
-
     public class GetterStringWithEvenSymbolsTests
     {
         [Fact]
-        public void GetEvenPositionsStringBuilder_IsCorrectWorkOfMethod()
+        public void GetEvenPositionsStringBuilder_IsCorrectWorkOfMethodWithEvenLength()
         {
             StringBuilder inputString = new StringBuilder("qwqwqw");
+            string expected = "qqq";
+
+            GetterStringWithEvenSymbols evener = new GetterStringWithEvenSymbols(inputString);
+            string actual = evener.GetEvenPositionsStringBuilder().ToString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetEvenPositionsStringBuilder_IsCorrectWorkOfMethodWithOddLength()
+        {
+            StringBuilder inputString = new StringBuilder("qwqwq");
             string expected = "qqq";
 
             GetterStringWithEvenSymbols evener = new GetterStringWithEvenSymbols(inputString);
